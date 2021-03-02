450353f5b5081f1b4e2a54702dcfc72d82f3a5cb
Arthur Matias <ahmmfdc@gmail.com>
2021-03-02 06:42:02 +0000

    formatting color slider

--- src/components/ColorSlider.Component.svelte
+++ src/components/ColorSlider.Component.svelte
@@ -1,29 +1,29 @@
-<script lang='ts'>
-    export let color:string = `hsl(0,100%,41%)`;
-    export let secondaryColor:string = `hsl(0,100%,28%)`;
-    let colorValue:string = '0';
+<script lang="ts">
+    export let color: string = `hsl(0,100%,41%)`;
+    export let secondaryColor: string = `hsl(0,100%,28%)`;
+    let colorValue: string = "0";
 
-    const setColor = (value: string)=>{
+    const setColor = (value: string) => {
         color = `hsl(${value},100%,41%)`;
         secondaryColor = `hsl(${value},100%,28%)`;
     };
-    function handleChange(e: Event ) {
-        let target = e.target as HTMLInputElement
+    function handleChange(e: Event) {
+        let target = e.target as HTMLInputElement;
         let value = target.value;
-        console.log(target.value)
-        console.log(color)
+        console.log(target.value);
+        console.log(color);
         setColor(value);
-    };
-    const getGradient = () => {
-        let allColorsGradient = 'linear-gradient(to right, hsl(0,100%,41%),';
-        for (let i = 1; i < 360; i++) {
-            allColorsGradient += `hsl(${i},100%,41%),`
-        }
-        allColorsGradient+='hsl(360,100%,41%))'
-        return allColorsGradient;
     }
+    const getGradient = () => {
+        let allColorsGradient = "linear-gradient(to right, hsl(0,100%,41%),";
+        for (let i = 1; i < 360; i++) {
+            allColorsGradient += `hsl(${i},100%,41%),`;
+        }
+        allColorsGradient += "hsl(360,100%,41%))";
+        return allColorsGradient;
+    };
     let gradient = getGradient();
-    var sheet = document.createElement('style')
+    var sheet = document.createElement("style");
     sheet.innerHTML = `
     input[type=range]{
         -webkit-appearance: none;
@@ -50,46 +50,47 @@
         border: none;
     }
 
-input[type=range]::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    border: none;
-    height: 2rem;
-    width: 2rem;
-    border-radius: 50%;
-    background: var(--current-color);
-    transition: ease .2ms;
-    cursor: pointer;
+    input[type=range]::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        border: none;
+        height: 2rem;
+        width: 2rem;
+        border-radius: 50%;
+        background: var(--current-color);
+        transition: ease .2ms;
+        cursor: pointer;
 
-    margin-top: -.7rem;
-}
-input[type=range]::-webkit-slider-thumb:hover{
-    filter: brightness(.8)
-}
-input[type=range]:focus {
-    outline: none;
-}
+        margin-top: -.7rem;
+    }
+    input[type=range]::-webkit-slider-thumb:hover{
+        filter: brightness(.8)
+    }
+    input[type=range]:focus {
+        outline: none;
+    }
 
-input[type=range]:focus::-webkit-slider-runnable-track {
-    background: ${gradient};
-}
+    input[type=range]:focus::-webkit-slider-runnable-track {
+        background: ${gradient};
+    }
     `;
     document.body.appendChild(sheet);
 </script>
 
 <div class="slider-container" style={`--current-color: ${color};`}>
-    <input 
-    type="range" 
-    name="colorSlider" 
-    start='0' 
-    min='0' max='360' 
-    id="colorSlider" 
-    on:change={(e)=>handleChange(e)}
-    value={colorValue}
+    <input
+        type="range"
+        name="colorSlider"
+        start="0"
+        min="0"
+        max="360"
+        id="colorSlider"
+        on:change={(e) => handleChange(e)}
+        value={colorValue}
     />
 </div>
 
 <style>
-    .slider-container{
+    .slider-container {
         display: flex;
         align-items: center;
         justify-content: center;
